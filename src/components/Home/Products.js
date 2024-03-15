2387070b61118f445ccdfb6b2052b01bd211a8a8
ishu788 <ishu788.is@gmail.com>
2024-03-14 22:24:05 -0700

    home page update

--- src/components/Home/Products.js
+++ src/components/Home/Products.js
@@ -1,16 +1,31 @@
 
 import { Container, Row, Col } from "react-bootstrap";
 function Product( {data}){
-    return (
+    const generateRandomPrice = () => {
+        // Define minimum and maximum price range
+        const minPrice = 10; // Minimum price
+        const maxPrice = 100; // Maximum price
     
+        // Generate a random price within the range
+        return (Math.random() * (maxPrice - minPrice) + minPrice).toFixed(2);
+      };
+    return (
         <Container fluid className="home-about-section" id="about">
-        <h2>Best Selling Products</h2>
         <div className="cards-container">
+        <div style={{minWidth:"-webkit-fill-available",paddingTop:"20px"}}><h2 style={{color:"white"}}>Best Selling Products</h2></div>
         {data.slice(1, 8).map((wine, index) => (
-        <div className="card" key={index}>
-        <img src={wine.image} alt={`alt for ${wine.wine}`} />
-        {/* <p>{wine.wine}</p> */}
-        </div>))}  
+            <div className="card" key={index}>
+                <div className="img-container">
+                <img src={wine.image} alt={`alt for ${wine.wine}`} />
+                </div>
+                <div className="name-container">
+                <p style={{color:"white"}}>{wine.wine}
+                <br></br>
+                <p style={{color:"red"}}>{wine.winery}</p></p>
+                <br></br>
+                <p  style={{color:"white",marginTop:"-30px",fontSize:"20px",fontWeight:"bold"}}>${generateRandomPrice()}</p>
+                </div>
+            </div>))}  
         </div>
         </Container>
 )
