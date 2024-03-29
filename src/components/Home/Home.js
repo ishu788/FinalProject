ac672afda83592eefa826d11318fdd365b509ee1
ishu788 <ishu788.is@gmail.com>
2024-03-29 13:17:19 -0700

    carousel changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,66 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Carousel } from "react-bootstrap";
 import LottieAnimation from "../Lottie"; // Assuming you have a file named Lottie.js
 import homesvg from "../../Assets/lottie1.json";
 
 import homeImage from "../../Assets/homewine.jpg";
+import homeImage2 from "../../Assets/homewine2.jpg";
+import homeImage3 from "../../Assets/Wines/red-wine-1129189_1920.jpg";
+
+
+
 import Product from "./Products";
 import Cards from "./Cards";
+
 
 function Home({ data }) {
   return (
     
     <div>
       <Container fluid className="home-section" id="home">
-      <section>
+
+      <Carousel fade interval={5000}> 
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={homeImage}
+            alt="First slide"
+            style={{
+              maxHeight: "700px",
+              width: "100%"
+            }}
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={homeImage2}
+            alt="Second slide"
+            style={{
+              maxHeight: "700px",
+              width: "100%"
+            }}
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={homeImage3}
+            alt="Third slide"
+            style={{
+              maxHeight: "700px",
+              width: "100%"
+            }}
+          />
+        </Carousel.Item>
+      </Carousel>
+      {/* <section>
       <img src={homeImage} className="img-fluid" alt="avatar" 
               style={{
                 maxHeight: "700px",
                width: "100%"
               }}/>
-      </section>
+      </section> */}
       </Container>
       <Product data = {data} className="mainDiv"/>
       {/* <Cards data={data}/> */}
